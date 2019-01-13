02bc4bf6f40d75c7e926744cfca7c4e9081875c7
leonardo15043 <lahernandez15043@gmail.com>
2019-01-13 15:30:12 -0500

    Crear estructura basica de servicio , crear interfaz de nuestro componente y traer datos del formulario a nuestro componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeroeComponent } from './components/heroes/heroe.component';
 
 import { APP_ROUTING } from './app.routes';
 
+import { HeroesService } from './services/heroes.service';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -16,9 +19,12 @@ import { APP_ROUTING } from './app.routes';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
